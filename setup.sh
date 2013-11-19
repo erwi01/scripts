70e173126cdbaff22993db3a11b336e7c3ee9005
erwi01 <erik.wiklund@genti.se>
2013-11-19 15:21:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+
+#set -x
+
+set -e
+
 sudo apt-get update
 
 sudo apt-get --yes --quiet install ntp
+
+mysql DEBIAN FRONTEND=noninteractive apt-get --yes --quiet install mysql-server mysql-client libmysql-java
+mysqladmin --user=root password foobar
+
+sudo service msql stop
+sudo sed --in-place=.orig --expression='s/^bind-address.*$/bind-address=0.0.0.0/' /etc/mysql/my.cnf
+sudo service mysql start
+sudo service mysql status
+
+mysql --user=root --password=foobar <<"EOT"
+create database DroidChat;
+create user 'droid'@'localhost' indetified by 'droid';
+grant all on DroidChat.* to 'droid'@'localhost';
+flush privileges;
+EOT
+mysql --user=droid --passord=droid --table --execute='show databases;' DroidChat
 
